59aaebf7ca1fba3afa99fd9da2cae9a4ebed8df8
flaribbit <1149761294@qq.com>
2019-09-09 22:39:45 +0800

    update .gitignore

--- map/base.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="2019.06.04" name="base" tilewidth="16" tileheight="16" tilecount="144" columns="16">
- <image source="../img/Tileset.png" width="256" height="144"/>
-</tileset>
